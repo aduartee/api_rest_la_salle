0c0df868ac9821486aa5c859735143a5b719cef5
aduartee <arthurduartealves99@gmail.com>
2024-05-23 12:30:25 -0300

    feat: Change functions and create a README

---
+++ README.md
@@ -0,0 +1,28 @@
+### Welcome to my project
+---
+- In this project, I've created a simple API using Python, Flask, and MongoDB. 
+
+### Purpose of the API
+- This API serves as an endpoint for a mobile application. The application utilizes this API to manage questions and get correct responses.
+
+### What you find in this API?
+
+With this API, you can perform basic CRUD operations:
+
+    (C) - Create 
+    (R) - Read 
+    (U) - Update 
+    (D) - Delete 
+
+Getting Started
+
+To run the API locally, follow these steps:
+
+### References
+---
+- https://www.mongodb.com/docs/manual/tutorial/install-mongodb-on-ubuntu/#std-label-install-mdb-community-ubuntu
+- https://medium.com/featurepreneur/mongodb-crud-with-flask-e5700ad7996e
+- https://www.moesif.com/blog/technical/restful/Guide-to-Creating-RESTful-APIs-using-Python-Flask-and-MongoDB/
+- https://flask.palletsprojects.com/en/3.0.x/#api-reference
+- https://www.youtube.com/watch?v=6KscmgLZFic
+- https://www.geeksforgeeks.org/how-to-connect-mongodb-compass-to-flask/
--- rest_api.py
+++ rest_api.py
@@ -1,8 +1,8 @@
 # Importa as bibliotecas necessárias
-from flask import Flask
-from flask import request, jsonify
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 import pymongo
+from jsonschema import validate, ValidationError as JSONSchemaValidationError
 
 # Conexão com o MongoDB usando pymongo
 # myclientName é uma instância do cliente MongoDB, conectando-se ao servidor MongoDB rodando em localhost na porta 27017
@@ -24,25 +24,53 @@ cors = CORS(app)
 # Configura o cabeçalho de CORS
 app.config["CORS_HEADERS"] = "Content-type"
 
-@app.route('/Push', methods=['Post', 'GET'])
-def Push():
-    content = request.get_json()
-    collection.insert_one(content) 
+
+question_schema = {
+    "type": "object",
+    "properties": {
+        "title": {"type": "string"},
+        "questions": {
+            "type": "array",
+            "items": {"type": "string"},
+            "minItems": 3,
+            "maxItems": 3
+        },
+        "response": {"type": "integer", "minimum": 0, "maximum": 2}
+    },
+    "required": ["title", "questions", "response"]
+}
+
+@app.route('/questions', methods=['GET'])
+def get_questions():
+    questions = list(collection.find())
     
-@app.route('/Update', methods=['Post', 'GET'])
-def Update():
-    content = request.get_json()
-    collection.insert_one(content) 
+    for question in questions:
+        question['_id'] = str(question['_id'])
+        
+    return (jsonify(questions), 200)
     
-@app.route('/Delete', methods=['Post', 'GET'])
-def Delete():
-    content = request.get_json()
-    collection.insert_one(content) 
+@app.route('/questions', methods=['POST'])
+def add_question():
+    try:
+        question = request.get_json()
+        validate(instance=question, schema=question_schema)
     
-@app.route('/Pulling', methods=['Post', 'GET'])
-def Pulling():
-    content = request.get_json()
-    collection.insert_one(content) 
+    except JSONSchemaValidationError as e:
+        return jsonify({"error": str(e)}), 400
+
+    result = collection.insert_one(question)
+    return jsonify({"id": str(result.inserted_id)}), 201
     
+
+@app.route('/questions/<question_id>', methods=['GET'])
+def get_question_by_id(question_id):
+    question = collection.find_one({"_id": pymongo.ObjectId(question_id)})
+    
+    if not question:
+        return jsonify({"error": "Not Found this Question in Colletion"}), 404
+
+    question['_id'] = str(question['_id'])
+    return jsonify(question), 200
+
 if __name__ == "__Main__":
     app.run()
